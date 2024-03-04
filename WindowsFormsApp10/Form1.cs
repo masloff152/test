52882d7b70abff12c60e2b2bc2783f432ac4dbdf
Сергей Маслов <msl.ser152@yandex.ru>
2024-03-04 14:42:02 +0300

    FIRST COMMIT

---
+++ WindowsFormsApp10/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp10
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source = champsrv.nntc.nnov.ru; DataBase = user01; User Id = user01; Password = 83328;";
+        string connTable = "SELECT * FROM Human";   // TEEEEESt
+        public Form1()
+        {
+            InitializeComponent();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(connTable, conn);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+                conn.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
